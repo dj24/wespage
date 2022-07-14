b9170d664c1576686b7c057aef5383446bb30fad
dj24 <danieljackson97123@gmail.com>
2022-07-14 23:45:43 +0100

    try add fbx

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,60 @@
 import ReactDOM from "react-dom/client";
-import { Canvas, useThree } from "@react-three/fiber";
+import { Suspense } from "react";
 import { motion } from "framer-motion-3d";
-import { useViewportScroll } from "framer-motion";
-// import vector3 from 'three'
+import { useScroll } from "framer-motion";
+import { Canvas, useThree, useLoader } from "@react-three/fiber";
+import { useFBX } from "@react-three/drei";
+import { FBXLoader } from "../node_modules/three/examples/jsm/loaders/FBXLoader";
 
 import "./index.css";
-import { useEffect, useState } from "react";
+
+const Model = (props) => {
+  // const fbx = useFBX("/models/LeatherChair.fbx");
+  const fbx = useLoader(FBXLoader, "models/LeatherChair.fbx");
+  return <primitive object={fbx} />;
+};
 
 const Scene = () => {
+  const camera = useThree((state) => state.camera);
+  const { scrollYProgress } = useScroll();
+
+  scrollYProgress.onChange((value) => {
+    camera.position.set(0, -value * 20, 5);
+  });
+
   return (
     <>
       <ambientLight intensity={0.1} />
       <directionalLight color="red" position={[0, 0, 5]} />
-      <motion.mesh whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+      <motion.mesh
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        position={[0, 0, 0]}
+      >
         <boxGeometry args={[5, 5, 1]} />
         <meshStandardMaterial />
       </motion.mesh>
+      <motion.mesh
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        position={[0, -20, 0]}
+      >
+        <boxGeometry args={[5, 5, 1]} />
+        <meshStandardMaterial />
+      </motion.mesh>
+      {/*<Model />*/}
     </>
   );
 };
 
 const App = () => {
-  const { scrollYProgress } = useViewportScroll();
-  const [yPosition, setYPosition] = useState(0);
-  scrollYProgress.onChange((value) => {
-    setYPosition(value * 100);
-  });
-  useEffect(() => {
-    console.log(yPosition);
-  }, [yPosition]);
   return (
     <>
       <div id="canvas-container">
-        <Canvas camera={{ fov: 80, position: [0, yPosition, 10] }}>
-          <Scene />
+        <Canvas>
+          <Suspense fallback={null}>
+            <Scene />
+          </Suspense>
         </Canvas>
       </div>
       <div id="html-container">
